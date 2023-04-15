8aea408c26960a5876c75067076695e5fd1562a5
Denys Morozov <ajax3101@gmail.com>
2023-04-15 10:42:19 +0300

    first commint

---
+++ todo/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.models import User
+from django.db import IntegrityError
+from django.contrib.auth import login, logout, authenticate
+from .forms import TodoForm
+from .models import Todo
+
+
+def home(request):
+    return render(request, 'todo/home.html')
